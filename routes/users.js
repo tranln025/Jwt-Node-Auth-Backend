166d0c53c0c073ad29bbd0854fc4476228702298
daltonhart <daltonvaren@gmail.com>
2019-08-27 11:59:14 -0700

    adjusts user route to remove id requirement

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const ctrl = require("../controllers");
 const authRequired = require("../middleware/authRequired");
 
-router.get("/:id", authRequired, ctrl.users.show);
+router.get("/", authRequired, ctrl.users.show);
 
 module.exports = router;
